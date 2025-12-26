97d50e8aabe542748d5492a3607a21f008ad0b36
AristovaAlina <alinaaristova72@gmail.com>
2025-12-27 01:46:46 +0500

    Task1

--- Tyuiu.AristovaAK.Sprint6.Task1.V6.Lib/DataService.cs
+++ Tyuiu.AristovaAK.Sprint6.Task1.V6.Lib/DataService.cs
@@ -5,20 +5,16 @@ namespace Tyuiu.AristovaAK.Sprint6.Task1.V6.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            double[] res = new double[Math.Abs(startValue) + Math.Abs(stopValue)];
-            for (int i = 0; i < Math.Abs(startValue) + Math.Abs(stopValue); i++)
+            int count = stopValue - startValue + 1;
+            double[] res = new double[count];
+
+            for (int i = 0; i < count; i++)
             {
-                if ((3 * startValue - 1) == 0)
-                {
-                    res[i] = 0;
-                }
-                else
-                {
-                    double r = Math.Round(((Math.Sin(startValue) - 2 * startValue) / (3 * startValue - 1)) + Math.Sin(startValue) - 3 * startValue + 2, 2);
-                    res[i] = r;
-                }
-                startValue++;
+                double x = startValue + i;
+                double result = ((Math.Sin(x) - 2 * x) / (3 * x - 1)) + Math.Sin(x) - 3 * x + 2;
+                res[i] = Math.Round(result, 2);
             }
+
             return res;
         }
     }
--- Tyuiu.AristovaAK.Sprint6.Task1.V6.Test/DataServiceTest.cs
+++ Tyuiu.AristovaAK.Sprint6.Task1.V6.Test/DataServiceTest.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.AristovaAK.Sprint6.Task1.V6.Test
         {
             DataService ds = new DataService();
             double[] res = ds.GetMassFunction(-5, 5);
-            double[] wait = { 17.27, 14.08, 10.27, 6.65, 3.87, 2, -0.74, -3.7, -7.59, -11.55, -14.74 };
+            double[] wait = { 17.27, 14.08, 10.27, 6.65, 3.87, 2.0, -0.74, -3.71, -7.59, -11.55, -14.74 };
 
             CollectionAssert.AreEqual(wait, res);
         }
